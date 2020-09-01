0818f91ebac6e4132db84b74200f1788eee693de
Landon Higinbotham <lch43@pitt.edu>
2020-09-01 18:46:45 -0400

    Ready for submission. Not sure if everything is 100% correct, but I am going for it atm.

--- lab2/test.c
+++ lab2/test.c
@@ -39,20 +39,4 @@ int main(void)
     printf("\n");
     print_v4(mat4_mult_v4(m1, v1));
     printf("\n");
-
-
-    mat4 minvTest = {
-        (vec4){-4, -1, -4, -1},
-        (vec4){0, 2, 4, -9},
-        (vec4){0, 0, 4, -1},
-        (vec4){0, 0, 0, 1}
-    };
-
-    print_mat4(minvTest);
-    printf("\n");
-
-    print_mat4(inv_mat4(minvTest));
-    printf("\n");
-
-    print_mat4(mat4_mult_mat4(minvTest, inv_mat4(minvTest)));
 }
