a9d3c6b3e36e9e9552c7dba2c756af6c3ccdbfac
Landon Higinbotham <lch43@pitt.edu>
2020-09-18 02:39:03 -0400

    Fixed problem with small angles and changed color to random instead of rainbow. Left rainbow functionality though.

--- src/lab3/lab03.c
+++ src/lab3/lab03.c
@@ -4,6 +4,7 @@
  * An OpenGL source code template.
  */
 
+/* Modified by Landon Higinbotham to create a cone and color it. */
 
 #include <GL/glew.h>
 #include <GL/freeglut.h>
@@ -17,8 +18,10 @@
 
 #define BUFFER_OFFSET( offset )   ((GLvoid*) (offset))
 
+/*Landon Higinbotham's code starts here*/
 /* For now we can keep this constant since we will not be changing it*/
-const int coneSides = 20;
+const int coneSides = 100;
+/*Vertices = number of vertical sides of the cone * 2 since we must create the bottom faces, and * 3 because each triangle has 3 vertices.*/
 int num_vertices = coneSides * 2 * 3;
 
 /*
@@ -29,20 +32,23 @@ int num_vertices = coneSides * 2 * 3;
 */
 void createCone(float tipY, float height, float width, vec4 * vertices)
 {
-    int vertex;
-    double angle = 360/coneSides;
+    int side;
+    /*Take degrees of circle because it is easy for me to understand this way.*/
+    double angle = 360.0/coneSides;
     float heightOffset = tipY - height;
-    for (vertex = 0; vertex<coneSides; vertex++)
+    for (side = 0; side<coneSides; side++)
     {
-        int vertexOffset = vertex * 6;
-        float rad1 = (angle * vertex) * M_PI/180;
-        float rad2 = (angle * (vertex+1)) * M_PI/180;
+        int vertexOffset = side * 6;
+        /*Gain the radians needed to create the triangle*/
+        float rad1 = (angle * side) * M_PI/180.0;
+        float rad2 = (angle * (side+1)) * M_PI/180.0;
 
+        /*Using the radians, find the X and Y's of the bottom points.*/
         float base1X = (float) (width * sin(rad1));
         float base1Z = (float) (width * cos(rad1));
         float base2X = (float) (width * sin(rad2));
         float base2Z = (float) (width * cos(rad2));
-        
+
         /*Vertical sides*/
         vertices[vertexOffset] = (vec4) {0.0, tipY, 0.0 , 1.0};
         vertices[vertexOffset+1] = (vec4) {base1X, heightOffset, base1Z, 1.0};
@@ -52,10 +58,35 @@ void createCone(float tipY, float height, float width, vec4 * vertices)
         vertices[vertexOffset+3] = (vec4) {0.0, heightOffset, 0.0, 1.0};
         vertices[vertexOffset+4] = (vec4) {base1X, heightOffset, base1Z, 1.0};
         vertices[vertexOffset+5] = (vec4) {base2X, heightOffset, base2Z, 1.0};
+        
     }
 }
 
 void colorCone(vec4 * colors)
+{
+    int side;
+    for (side = 0; side<coneSides; side++)
+    {
+        /*Choose random colors.*/
+        float ColorR = rand() % 256 / 255.0;
+        float ColorG = rand() % 256 / 255.0;
+        float ColorB = rand() % 256 / 255.0;
+        
+        int vertexOffset = side * 6;
+
+        /*Make vertical side colors*/
+        colors[vertexOffset] = (vec4) {ColorR,ColorG,ColorB,1.0};
+        colors[vertexOffset+1] = (vec4) {ColorR,ColorG,ColorB,1.0};
+        colors[vertexOffset+2] = (vec4) {ColorR,ColorG,ColorB,1.0};
+
+        /*Just set the bottom to white*/
+        colors[vertexOffset+3] = (vec4) {1.0,1.0,1.0,1.0};
+        colors[vertexOffset+4] = (vec4) {1.0,1.0,1.0,1.0};
+        colors[vertexOffset+5] = (vec4) {1.0,1.0,1.0,1.0};
+    }
+}
+
+void rainbowColorCone(vec4 * colors)
 {
     int side;
     float colorVariation = 765.00 / (coneSides);
@@ -69,22 +100,22 @@ void colorCone(vec4 * colors)
         {
             if (currentColor >= 510)
             {
-                ColorR = (0 + (currentColor-510))/255;
-                ColorG = 0;
-                ColorB = (255 - (currentColor-510))/255;
+                ColorR = (0.0 + (currentColor-510.0))/255.0;
+                ColorG = 0.0;
+                ColorB = (255.0 - (currentColor-510.0))/255.0;
             }
             else
             {
-                ColorR = 0;
-                ColorG = (255 - (currentColor-255))/255;
-                ColorB = (0 + (currentColor-255))/255;
+                ColorR = 0.0;
+                ColorG = (255.0 - (currentColor-255.0))/255.0;
+                ColorB = (0.0 + (currentColor-255.0))/255.0;
             }
         }
         else
         {
-            ColorR = (255 - currentColor)/255;
-            ColorG = (0 + currentColor)/255;
-            ColorB = 0;
+            ColorR = (255.0 - currentColor)/255.0;
+            ColorG = (0.0 + currentColor)/255.0;
+            ColorB = 0.0;
         }
         
         int vertexOffset = side * 6;
@@ -95,18 +126,24 @@ void colorCone(vec4 * colors)
         colors[vertexOffset+2] = (vec4) {ColorR,ColorG,ColorB,1.0};
 
         /*Just set the bottom to white*/
-        colors[vertexOffset+3] = (vec4) {255,255,255,1.0};
-        colors[vertexOffset+4] = (vec4) {255,255,255,1.0};
-        colors[vertexOffset+5] = (vec4) {255,255,255,1.0};
+        colors[vertexOffset+3] = (vec4) {1.0,1.0,1.0,1.0};
+        colors[vertexOffset+4] = (vec4) {1.0,1.0,1.0,1.0};
+        colors[vertexOffset+5] = (vec4) {1.0,1.0,1.0,1.0};
     }
 }
-
+/*Landon Higinbotham's code ends here*/
 void init(void)
 {
+    /*Landon Higinbotham's code starts here*/
+    /*Create the vertices array with the appropriate size*/
     vec4 vertices[num_vertices];
+    /*Populate vertices array*/
     createCone(0.5, 1, .5, vertices);
+    /*Create the colors array with the appropriate size*/
     vec4 colors[num_vertices];
+    /*Populate colors array*/
     colorCone(colors);
+    /*Landon Higinbotham's code ends here*/
 
     GLuint program = initShader("vshader.glsl", "fshader.glsl");
     glUseProgram(program);
